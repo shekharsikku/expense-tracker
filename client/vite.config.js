58b2747bc7a5052ec6cfb16d8f92c44b4e0ed824
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-03-11 15:17:51 +0530

    All UI Components Created

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/graphql': 'http://localhost:4000',
+    },
+    port: 3000,
+  },
+  preview: {
+    proxy: {
+      '/graphql': 'http://localhost:4000',
+    },
+    port: 3000,
+  },
   plugins: [react()],
-})
+});
